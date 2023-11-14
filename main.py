a765b1539a241e12fc8c8976bc8a4513f71be66f
kubarada <jakub.rd@seznam.cz>
2023-11-14 19:45:50 +0100

    last figs + final version

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ defib_strength = 2
 
 sol_no_stim = odeint(simulation.van_der_pol_oscillator_no_stim, x0, t, args=(mu,))
 
-# graf bez stimulace
 plt.plot(t, sol_no_stim[:, 0])
 plt.xlabel('t [s]')
 plt.ylabel(r'$x_1(t)$')
@@ -24,7 +23,6 @@ plt.show()
 
 sol_arrhythmia = odeint(simulation.van_der_pol_arrhythmia, x0, t, args=(mu, arrhythmia_factor))
 
-# Plot results with arrhythmia simulation
 plt.plot(t, sol_arrhythmia[:, 0])
 plt.xlabel('t [s]')
 plt.ylabel(r'$x_1(t)$')
@@ -34,17 +32,15 @@ plt.show()
 
 sol_arrhythmia_stimulator = odeint(simulation.van_der_pol_arrhythmia_stimulator, x0, t, args=(mu, arrhythmia_factor, simulation.cardiac_stimulator))
 
-# Plot results with arrhythmia and stimulator
 plt.plot(t, sol_arrhythmia_stimulator[:, 0])
 plt.xlabel('t [s]')
 plt.grid()
-plt.ylabel(r'$x_1(t)$')  # Using LaTeX for the y-axis label
+plt.ylabel(r'$x_1(t)$')
 plt.title('Činnost srdce s arytmií regulována kardiostimulátorem')
 plt.show()
 
 sol_cardiac_arrest = odeint(simulation.van_der_pol_cardiac_arrest, x0, t, args=(mu, arrest_time))
 
-# Plot results with cardiac arrest simulation
 plt.plot(t, sol_cardiac_arrest[:, 0])
 plt.xlabel('t [s]')
 plt.ylabel(r'$x_1(t)$')
@@ -52,10 +48,8 @@ plt.grid()
 plt.title('Srdeční zástava')
 plt.show()
 
-# Solve ODE with cardiac arrest and defibrillator
 sol_defibrillator = odeint(simulation.van_der_pol_defibrillator, x0, t, args=(mu, arrest_time, defib_time, defib_strength))
 
-# Plot results with cardiac arrest and defibrillator
 plt.plot(t, sol_defibrillator[:, 0])
 plt.xlabel('t [s]')
 plt.ylabel(r'$x_1(t)$')
--- simulation.py
+++ simulation.py
@@ -15,37 +15,35 @@ def van_der_pol_arrhythmia(x, t, mu, arrhythmia_factor):
     dxdt = [x2, mu*(1 - x1**2)*x2 - x1]
     return dxdt
 
+# Van der Pol oscilátor se stimulátorem
 def van_der_pol_arrhythmia_stimulator(x, t, mu, arrhythmia_factor, I):
     x1, x2 = x
-    # Introduce arrhythmia by adding a time-varying factor to the mu parameter
     mu += arrhythmia_factor * np.sin(0.5 * t)
     dxdt = [x2, mu*(1 - x1**2)*x2 - x1 + I(t)]
     return dxdt
 
-# Cardiac stimulator function
+# kardiostimulátor
 def cardiac_stimulator(t):
-    # Simple stimulator: periodic pulses
-    if t % 5 == 0:  # Stimulate at a regular interval (e.g., every 10 seconds)
-        return 10  # Strength of the stimulation
+    if t % 5 == 0:
+        return 10
     else:
         return 0
 
+# simulátor zástavy srdce
 def van_der_pol_cardiac_arrest(x, t, mu, arrest_time):
     x1, x2 = x
     if t >= arrest_time:
-        # Simulate cardiac arrest by setting the system to a stable state
         dxdt = [0, 0]
     else:
         dxdt = [x2, mu*(1 - x1**2)*x2 - x1]
     return dxdt
 
+# simulace zástavy srdce a následné defibrilace
 def van_der_pol_defibrillator(x, t, mu, arrest_time, defib_time, defib_strength):
     x1, x2 = x
     if t > arrest_time:
-        # Simulate cardiac arrest
         dxdt = [0, 0]
         if t > defib_time:
-            # Simulate defibrillation
             dxdt = [defib_strength, 0]
     else:
         dxdt = [x2, mu*(1 - x1**2)*x2 - x1]
